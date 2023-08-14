55ec9adf3c8ccae195fbb2d5f5f32e2afa8dc06a
David KENEY <messan-david.keney@capgemini.com>
2023-08-14 16:34:56 +0200

    my-ecommerce-app : router && hooks + refactoring

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,39 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
 
-export const Navbar = ({ filter, setFiltering }) => {
+export const Navbar = ({ filter, setFiltering, count }) => {
   return (
-    <nav className='navbar orange navbar-expand-lg navbar-light bg-light fixed-top'>
-      <a href='' className='navbar-brand crimson'>
-        <i className='fas fa-shopping-cart'></i> Mes Courses en Ligne
-      </a>
+    <nav className="navbar orange navbar-expand-lg navbar-light bg-light fixed-top">
+        <Link  className="navbar-brand crimson" to="/">   <i class="fas fa-shopping-cart"></i> Mes Courses en Ligne</Link>
       <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarSupportedContent'
-        aria-controls='navbarSupportedContent'
-        aria-expanded='false'
-        aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon'></span>
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+      <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <div className='ml-auto cart'>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="ml-auto cart">
           <div>
-            <form className='search form-inline my-2 my-lg-0'>
+            <form className="search form-inline my-2 my-lg-0">
               <input
-                className='form-control mr-sm-2'
-                type='search'
-                placeholder='Search'
-                aria-label='Search'
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
                 onChange={(e) => {
-                  setFiltering(e.target.value.length > 0);
-                  filter(e.target.value);
+                  setFiltering(e.target.value.length > 0)
+                  filter(e.target.value)
                 }}
-              />
+                />
             </form>
           </div>
-          <div className='menu-right'>{/* cart */}</div>
+          <div className="menu-right">
+             <Link to="/cart">
+                <i class="fas fa-shopping-bag fa-2x grey"></i>
+             </Link>
+              <span class="badge badge-pill badge-success">{count}</span>
+          </div>
         </div>
       </div>
     </nav>
@@ -43,95 +48,104 @@ export const Navbar = ({ filter, setFiltering }) => {
 export const Footer = () => {
   return (
     <footer>
-      <div className='container'>
-        <span className='text-muted'>Place sticky footer content here.</span>
+      <div className="container">
+        <span className="text-muted">Place sticky footer content here.</span>
       </div>
     </footer>
   );
 };
 
-export const Card = (props) => {
-  const { item } = props;
+export const Card = props => {
+  const { item, addToCart, count } = props
   return (
-    <div className='col-sm-4'>
-      <div className='card'>
+    <div className="col-sm-4">
+      <div className="card">
         <img
-          width='170'
-          height='170'
-          src={
-            process.env.PUBLIC_URL + `/assets/${item.category}/${item.image}`
-          }
+          width="170"
+          height="170"
+          src={process.env.PUBLIC_URL + `/assets/${item.category}/${item.image}`}
           alt={item.name}
         />
-        <div className='card-body'>
-          <div className='row'>
-            <div className='col-sm-6'>
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-6">
               <h4>{item.name}</h4>
             </div>
-            <div className='col-sm-6'>
+            <div className="col-sm-6">
               <p>
-                €{item.price}/{item.unit}
+                  €{item.price}/{item.unit}
               </p>
-              <button className='btn btn-warning btn-sm'>view product</button>
+              <button className="btn btn-warning btn-sm" data-toggle="modal" data-target={`#${item.ref}`}>view product</button>
             </div>
           </div>
         </div>
       </div>
-      {/* modal */}
+      <Modal item={item} addToCart={addToCart} count={count}/>
     </div>
   );
 };
 
-export const Modal = () => {
+
+export const Modal = ({ item, addToCart, count }) => {
+  const [qty, setQty] = useState(1);
   return (
     <div
-      className='modal fade '
-      id=''
-      data-backdrop='static'
-      tabindex='-1'
-      role='dialog'
-      aria-labelledby='staticBackdropLabel'
-      aria-hidden='true'>
-      <div className='modal-dialog modal-xl' role='document'>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h5 className='modal-title' id='staticBackdropLabel'>
-              Citrons
-            </h5>
+      class="modal fade "
+      id={item.ref}
+      data-backdrop="static"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">{item.name}</h5>
             <button
-              type='button'
-              className='close'
-              data-dismiss='modal'
-              aria-label='Close'>
-              <span aria-hidden='true'>&times;</span>
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div className='modal-body'>
-            <div className='row'>
-              <div className='col-sm-4'>
+          <div class="modal-body">
+            <div className="row">
+              <div className="col-sm-4">
                 <img
-                  width='170'
-                  height='170'
-                  src={process.env.PUBLIC_URL + `/assets/0/citron.png`}
-                  alt='Citron'
+                  width="170"
+                  height="170"
+                  src={
+                    process.env.PUBLIC_URL +
+                    `/assets/${item.category}/${item.image}`
+                  }
+                  alt="Citron"
                 />
               </div>
 
-              <div className='col-sm'>
-                <p className='lead'>
+              <div className="col-sm">
+                <p class="lead">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore
                 </p>
-                <h3 className='price'>€1.99</h3> <br />
+                  <h3 className="price">€{item.price}/{item.unit}</h3> <br />
                 <div
-                  className='btn-group'
-                  role='group'
-                  aria-label='Basic example'>
-                  <button type='button' className='btn btn-secondary'>
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic example"
+                >
+                  <button
+                    onClick={() => setQty(count > 1 ? count - 1 : 1)}
+                    type="button"
+                    className="btn btn-secondary">
                     -
                   </button>
-                  <span className='btn btn-light qty'>1</span>
-                  <button type='button' className='btn btn-secondary'>
+                    <span className="btn btn-light qty">{qty}</span>
+                  <button
+                    onClick={() => setQty(count + 1)}
+                    type="button"
+                    className="btn btn-secondary">
                     +
                   </button>
                 </div>
@@ -140,17 +154,19 @@ export const Modal = () => {
             </div>
           </div>
 
-          <div className='modal-footer'>
+          <div class="modal-footer">
             <button
-              type='button'
-              className='btn btn-secondary'
-              data-dismiss='modal'>
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal">
               Close
             </button>
             <button
-              type='button'
-              className='btn btn-success'
-              data-dismiss='modal'>
+              type="button"
+              class="btn btn-success"
+              data-dismiss="modal"
+              onClick={()=> addToCart(count + 1)}
+            >
               Add to Cart
             </button>
           </div>
@@ -160,14 +176,14 @@ export const Modal = () => {
   );
 };
 
-export const List = (props) => {
-  const { data, category } = props;
+
+export const List = props => {
+  const { data, category, addToCart, count } = props
+
   return (
-    <div className='col-sm'>
-      <div className='row'>
-        {data.map((item) => (
-          <Card key={item.ref} item={item} />
-        ))}
+    <div className="col-sm">
+      <div className="row">
+        {data.map(item => <Card key={item.ref} item={item} addToCart={addToCart} count={count}/>)}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './app/views/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
